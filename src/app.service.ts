14b1686c9934a62f4c56c6d9cb71e3b897ad653b
Sajjad Ali <sajjad.ali@badriconsultancy.com>
2025-08-05 18:00:56 +0500

    Minor adjustment to the default controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<h1>Hello World! This is a basic application</h1>';
   }
 }
