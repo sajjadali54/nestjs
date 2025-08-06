22d8146c8c6fa7e65b1547c3f0e8b0af68e17061
Sajjad Ali <sajjad.ali@badriconsultancy.com>
2025-08-06 10:13:42 +0500

    Post routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Post, Redirect, Req } from '@nestjs/common';
 
 import type { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
+  @Redirect('https://nestjs.com', 301)
   findAll(@Req() request: Request): string {
     console.log(request.query);
     return 'This action returns all cats';
+  }
+
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
   }
 }
