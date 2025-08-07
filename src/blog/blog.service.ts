2c9f6f301f255d08f5ec4e03e9bf2a5f6e78a00a
Sajjad Ali <sajjad.ali@badriconsultancy.com>
2025-08-07 12:42:49 +0500

    Functionality for service

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
 
+interface BlogPost {
+  id: number;
+  title: string;
+  content: string;
+}
+
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  private readonly blogPosts: BlogPost[] = [];
+
+  create(blogPost: BlogPost) {
+    const lastId = this.blogPosts[this.blogPosts.length - 1]?.id || 0;
+    const post = {
+      title: blogPost.title,
+      content: blogPost.content,
+      id: lastId + 1,
+    };
+    this.blogPosts.push(post);
+    return post;
+  }
+
+  findAll(): BlogPost[] {
+    return this.blogPosts;
+  }
+
+  findOne(id: number): BlogPost | undefined {
+    return this.blogPosts.find((post) => post.id === id);
+  }
+
+  update(id: number, updatedPost: Partial<BlogPost>): BlogPost | undefined {
+    const postIndex = this.blogPosts.findIndex((post) => post.id === id);
+    if (postIndex === -1) return undefined;
+
+    this.blogPosts[postIndex] = {
+      title: updatedPost.title || this.blogPosts[postIndex].title,
+      content: updatedPost.content || this.blogPosts[postIndex].content,
+      id: this.blogPosts[postIndex].id,
+    };
+    return this.blogPosts[postIndex];
+  }
+
+  remove(id: number): boolean {
+    const postIndex = this.blogPosts.findIndex((post) => post.id === id);
+    if (postIndex === -1) return false;
+    this.blogPosts.splice(postIndex, 1);
+    return true;
+  }
+}
