edee22e7b461fbf8419f7acedff853f93698f59a
Sajjad Ali <sajjad.ali@badriconsultancy.com>
2025-08-07 12:33:19 +0500

    Basic Module, Controller and Service for blog using nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { CatsModule } from './cats/cats.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
